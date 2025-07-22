b4dc72fcb34b4fe6ee5691f7e0659495b36f23b2
Pranta Barua <105166631+PB-King-47@users.noreply.github.com>
2025-07-23 00:31:22 +0600

    The Splash Screen Done, fix the recipe details margin size to see the end of the details

---
+++ app/src/main/java/com/example/cookingrecipeapplication/SplashActivity.java
@@ -0,0 +1,55 @@
+package com.example.cookingrecipeapplication;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    private static final int SPLASH_DURATION = 3000; // 3 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activity);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // prevent back to splash
+        }, SPLASH_DURATION);
+    }
+}
+
+/*
+
+package com.rohitanabhavane.learnguru;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+
+    }
+
+}
+
+
+*/
